acc5aaf01c3b6bf8064bcb9bd6978658ef724806
Piotrek Marciniak <piotrek@ptrm.eu>
2020-04-30 16:35:05 +0200

    Attempt a better failure detection for both gotty and tty ui
    Change-type: patch

--- ui/start.sh
+++ ui/start.sh
@@ -1,10 +1,37 @@
 #! /bin/bash
 
 # start gotty with boinctui
-/usr/app/gotty -w -p 80 boinctui&
+gotty_pid=''
+gotty_port=80
+
+start_webui () {
+  /usr/app/gotty -w -p $gotty_port boinctui&
+  gotty_pid="$!"
+}
+
+kill_webui () {
+  [ -z "$gotty_pid" ] && kill -9 $gotty_pid
+}
+
+while true; do
+    sleep 30s
+
+    curl -sf -o /dev/null http://localhost:$gotty_port
+
+    if [[ $? -ne 0 ]]; then
+      ps -p $gotty_pid && {
+        >&2 echo "gotty is stalled - restarting";
+        kill_webui;
+        start_webui;
+      } || {
+        >&2 echo "gotty exited - restarting";
+        start_webui;
+      }
+    fi
+done &
 
 export NCURSES_NO_UTF8_ACS=1
 #switch to tty2, clear screen and display boinctui 
 chvt 2
 printf "\033c" > /dev/tty2
-boinctui 2> /dev/null > /dev/tty2
+exec boinctui 2> /dev/null > /dev/tty2
