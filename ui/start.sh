8d8408b0fc7aba171adaf41ec640f216c553af71
Piotrek Marciniak <piotrek@ptrm.eu>
2020-05-01 20:40:25 +0200

    Run tty boinctui in the background

--- ui/start.sh
+++ ui/start.sh
@@ -5,7 +5,7 @@ export NCURSES_NO_UTF8_ACS=1
 #switch to tty2, clear screen and display boinctui 
 chvt 2
 printf "\033c" > /dev/tty2
-boinctui 2> /dev/null > /dev/tty2
+boinctui 2> /dev/null > /dev/tty2 &
 
 # start gotty with boinctui
 exec /usr/app/gotty -w -p 80 boinctui
