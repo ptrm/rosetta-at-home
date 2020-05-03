4c15dd234d312ca4bb89122fafc3c1e46f7b082d
Piotrek Marciniak <piotrek@ptrm.eu>
2020-05-03 11:45:34 +0200

    ui: revert to tty output as the main container process

--- ui/start.sh
+++ ui/start.sh
@@ -1,16 +1,10 @@
-#!/bin/bash
-
-export NCURSES_NO_UTF8_ACS=1
-
-start_tty2() {
-    sleep 12s
-    #switch to tty2, clear screen and display boinctui
-    chvt 2
-    printf "\033c" > /dev/tty2
-    boinctui 2> /dev/null > /dev/tty2
-}
-
-[ -z "$SKIP_BOINCTUI_TTY" ] && start_tty2 &
+#! /bin/bash
 
 # start gotty with boinctui
-exec /usr/app/gotty -w -p 80 boinctui
+/usr/app/gotty -w -p 80 boinctui&
+
+export NCURSES_NO_UTF8_ACS=1
+#switch to tty2, clear screen and display boinctui 
+chvt 2
+printf "\033c" > /dev/tty2
+boinctui 2> /dev/null > /dev/tty2
